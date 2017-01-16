1bf84dd11b3468cd2eb53a6d817642698e127199
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-01-16 16:28:16 +0900

    apply

--- init.zsh
+++ init.zsh
@@ -2,6 +2,11 @@
 
 export TEMPLATE_PLUGIN_ROOT="$(dirname ${(%):-%N})"
 
+function template-apply() {
+  cp ${TEMPLATE_PLUGIN_ROOT}/templates/${1}/* .
+  git init .
+}
+
 function template-list() {
   ls --color=no "${TEMPLATE_PLUGIN_ROOT}/templates/" | sed -e 's|/$||'
 }
